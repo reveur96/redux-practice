b255feac36e029e0d465df010b6ee929a675a241
reveur96 <dlfrlwkd11@daum.net>
2023-09-14 22:25:35 +0900

    increment & decrement function

--- redux-cart-function/src/components/Counter.js
+++ redux-cart-function/src/components/Counter.js
@@ -1,12 +1,38 @@
-import classes from './Counter.module.css';
+import {useSelector, useDispatch} from 'react-redux'
+import classes from "./Counter.module.css";
 
-const Counter = () => {
+const Counter = () =>
+{
+  const dispatch = useDispatch();
+  const counter = useSelector(state => state.counter);
+  
+  const incrementHandler = () =>
+  {
+    dispatch({type:'increment'})
+  };
+
+  const increaseHandler = () =>
+  {
+    dispatch({type:'increase' , amount: 10})
+  };
+
+  const decrementHandler = () =>
+  {
+    dispatch({type:'decrement'})
+    
+  }
+
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>increment</button>
+        <button onClick={increaseHandler}>increment by 10</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
---
+++ redux-cart-function/src/store/index.js
@@ -0,0 +1,30 @@
+import {legacy_createStore as createStore} from 'redux'
+
+const init = {counter: 0}
+const counterReducer = (state = init, action) =>
+{
+  if (action.type === 'increment')
+  {
+    return {
+      counter: state.counter + 1
+    }
+  }
+  if (action.type === 'increase')
+  {
+    return {
+    counter: state.counter + action.amount
+  }
+}
+  if (action.type === 'decrement')
+  {
+    return {
+      counter: state.counter - 1
+    }
+  }
+
+  return state
+};
+
+const store = createStore(counterReducer);
+
+export default store
