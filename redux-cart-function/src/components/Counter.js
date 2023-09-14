1d388ae1908bf172d67df9dec8ca358818a2007b
reveur96 <dlfrlwkd11@daum.net>
2023-09-14 22:30:41 +0900

    toggle function

--- redux-cart-function/src/components/Counter.js
+++ redux-cart-function/src/components/Counter.js
@@ -5,6 +5,7 @@ const Counter = () =>
 {
   const dispatch = useDispatch();
   const counter = useSelector(state => state.counter);
+  const showCounter = useSelector(state => state.showCounter);
   
   const incrementHandler = () =>
   {
@@ -22,12 +23,15 @@ const Counter = () =>
     
   }
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () =>
+  {
+    dispatch({type:'toggle'})
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {showCounter && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>increment</button>
         <button onClick={increaseHandler}>increment by 10</button>
--- redux-cart-function/src/store/index.js
+++ redux-cart-function/src/store/index.js
@@ -1,27 +1,38 @@
 import {legacy_createStore as createStore} from 'redux'
 
-const init = {counter: 0}
+const init = { counter: 0, showCounter: true }
 const counterReducer = (state = init, action) =>
 {
   if (action.type === 'increment')
   {
     return {
-      counter: state.counter + 1
+      counter: state.counter + 1,
+      showCounter: state.showCounter
     }
   }
   if (action.type === 'increase')
   {
     return {
-    counter: state.counter + action.amount
+      counter: state.counter + action.amount,
+      showCounter: state.showCounter
   }
 }
   if (action.type === 'decrement')
   {
     return {
-      counter: state.counter - 1
+      counter: state.counter - 1,
+      showCounter: state.showCounter
     }
   }
 
+  if (action.type === 'toggle')
+  {
+    return {
+      showCounter: !state.showCounter,
+      counter: state.counter
+    }
+  }
+  
   return state
 };
 
